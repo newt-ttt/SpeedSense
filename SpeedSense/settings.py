de9436ce0657e2e406954fb35ffa286f126590c1
newt-ttt <jawnsonlex@gmail.com>
2025-03-13 12:06:29 -0400

    Changed target tunnel

--- SpeedSense/settings.py
+++ SpeedSense/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "127.0.0.1", "speedsensesdp.pagekite.me"]
+ALLOWED_HOSTS = [ "127.0.0.1", "speedsensesdp.pagekite.me", "speedsense.pagekite.me"]
 
 
 # Application definition
